24794a0ada739557cfee36cb3945962e8f77e87e
HoyungBae <79411300+HoyungBae@users.noreply.github.com>
2021-02-22 01:29:57 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+
+
+client.once('ready',() =>{
+    console.log('Matatabi is online')
+});
+
+
+
+client.login('ODEyODU2OTg3NzAwMTY2Njg3.YDG2EA.neJ_0sS7puoa7jCbumEM4fRsmGU')
